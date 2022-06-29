2d211c498f7a27566e276cb3505b58b1f7541289
deloarts <philip.delorenzo+github@outlook.com>
2022-06-29 08:50:53 +0200

    Add number validator

---
+++ pytia_ui_tools/helper/__init__.py
@@ -0,0 +1,3 @@
+"""
+    Helper functions for pytia ui tools.
+"""
---
+++ pytia_ui_tools/helper/validators.py
@@ -0,0 +1,34 @@
+"""
+    Helper functions for validations.
+"""
+
+import re
+
+
+def validate_number(input: str, dot_only: bool = False) -> str:
+    """
+    Validates the input. Returns the validated number.
+
+    Args:
+        input (str): The input that needs validation.
+        dot_only (bool, optional): Replaces comma with dot. Defaults to False.
+
+    Returns:
+        Optional[str]: The validated input.
+    """
+    if dot_only:
+        value = input.replace(",", ".")
+    else:
+        value = input
+
+    if not re.match(r"(\d|\d.\d)$", value):
+        new_value = ""
+        floating_point = False
+        for character in value:
+            if character.isdigit():
+                new_value += character
+            if character in [".", ","] and not floating_point:
+                new_value += character
+                floating_point = True
+        return new_value
+    return input
