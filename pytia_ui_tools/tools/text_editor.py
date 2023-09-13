a5f81ebe5c0adb17f935535406806e39d2c4e343
Philip Delorenzo <git@deloarts.com>
2023-09-13 13:28:38 +0200

    Update styling

--- pytia_ui_tools/tools/text_editor.py
+++ pytia_ui_tools/tools/text_editor.py
@@ -2,15 +2,18 @@
     A simple text editor that accepts tkinter StringVars.
 """
 
-import tkinter as tk
-from tkinter import StringVar, Toplevel, font, scrolledtext, ttk
+from tkinter import END, WORD, StringVar
 from typing import Optional
+
+from ttkbootstrap import Button, Frame
+from ttkbootstrap.scrolled import ScrolledText
+from ttkbootstrap.window import Style, Toplevel
 
 from pytia_ui_tools.window_manager import WindowManager
 
 
 class TextEditor(Toplevel):
-    """Toplevel subclass that spawns a text editor window."""
+    """Toplevel subclass that spawns a text editor window with `ttkbootstrap` styling"""
 
     def __init__(
         self,
@@ -29,13 +32,12 @@ class TextEditor(Toplevel):
             height (int, optional): Window height. Defaults to 350.
         """
         Toplevel.__init__(self)
+        Style()
 
         self.textvariable = textvariable
 
         self.title(title if title else "Text Editor")
         self.attributes("-topmost", True)
-        self.default_font = font.nametofont("TkDefaultFont")
-        self.default_font.configure(family="Segoe UI", size=9)
         self.update()
 
         screen_width = self.winfo_screenwidth()
@@ -45,42 +47,39 @@ class TextEditor(Toplevel):
         self.geometry(f"{width}x{height}+{x_coordinate}+{y_coordinate}")
         self.minsize(width, height)
 
-        style = ttk.Style(self)
-        style.configure("Footer.TButton", width=14)
-
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
-        self.frame_data = ttk.Frame(master=self, style="Data.TFrame")
+        self.frame_data = Frame(master=self)
         self.frame_data.grid(row=0, column=0, sticky="nsew", padx=10, pady=(10, 5))
         self.frame_data.grid_columnconfigure(0, weight=1)
         self.frame_data.grid_rowconfigure(0, weight=1)
 
-        self.frame_footer = ttk.Frame(master=self, height=30, style="Footer.TFrame")
+        self.frame_footer = Frame(master=self, height=30)
         self.frame_footer.grid(row=1, column=0, sticky="swe", padx=10, pady=(5, 10))
         self.frame_footer.grid_columnconfigure(0, weight=1)
 
-        self.text_widget = scrolledtext.ScrolledText(
-            self.frame_data, font=("Segoe UI", 9), wrap=tk.WORD
-        )
+        self.text_widget = ScrolledText(self.frame_data, wrap=WORD)
         self.text_widget.grid(row=0, column=0, sticky="nsew")
         self.text_widget.focus_set()
-        self.text_widget.delete("1.0", tk.END)
-        self.text_widget.insert(tk.END, self.textvariable.get())
+        self.text_widget.text.delete("1.0", END)
+        self.text_widget.text.insert(END, self.textvariable.get())
 
-        self.btn_save = ttk.Button(
+        self.btn_save = Button(
             self.frame_footer,
-            text="Apply",
             command=self.on_btn_apply,
-            style="Footer.TButton",
+            text="Apply",
+            style="outline",
+            width=10,
         )
         self.btn_save.grid(row=0, column=0, padx=(5, 2), pady=0, sticky="e")
 
-        self.btn_abort = ttk.Button(
+        self.btn_abort = Button(
             self.frame_footer,
-            text="Abort",
             command=self.on_btn_abort,
-            style="Footer.TButton",
+            text="Abort",
+            style="outline",
+            width=10,
         )
         self.btn_abort.grid(row=0, column=1, padx=(2, 0), pady=0, sticky="e")
 
@@ -99,7 +98,7 @@ class TextEditor(Toplevel):
         Callback function for the apply button.
         Closes the window and writes the text value to the textvariable.
         """
-        self.textvariable.set(self.text_widget.get("1.0", tk.END))
+        self.textvariable.set(self.text_widget.text.get("1.0", END))
         self.grab_release()
         self.withdraw()
         self.destroy()
--- pytia_ui_tools/widgets/texts.py
+++ pytia_ui_tools/widgets/texts.py
@@ -2,23 +2,35 @@
     Tkinter text widgets.
 """
 
-import tkinter as tk
-from tkinter import StringVar, Toplevel, scrolledtext, ttk
+from tkinter import DISABLED, NORMAL, StringVar, Toplevel
 from typing import Optional
+
+from ttkbootstrap import Frame, Labelframe, Window, scrolled
 
 from pytia_ui_tools.tools.text_editor import TextEditor
 
 
-class ScrolledText(scrolledtext.ScrolledText):
-    """Text subclass widget with a vertical scrollbar."""
+class ScrolledText(scrolled.ScrolledText):
+    """
+    `ttkbootstrap` ScrolledText subclass widget with a vertical scrollbar and
+    textvariable support.
+    """
 
     def __init__(
         self,
         *args,
-        parent: tk.Tk | tk.Frame | ttk.Frame | ttk.Labelframe | Toplevel,
+        parent: Window | Frame | Labelframe | Toplevel,
         textvariable: Optional[StringVar] = None,
         **kwargs,
     ):
+        """Inits the widget.
+
+        Args:
+            parent (Window | Frame | Labelframe | Toplevel):  The `ttkbootstrap` \
+                master object.
+            textvariable (Optional[StringVar], optional): The tkinter textvariable. \
+                Defaults to None.
+        """
         self._textvariable = textvariable or StringVar()
 
         super().__init__(parent, *args, **kwargs)
@@ -26,51 +38,30 @@ class ScrolledText(scrolledtext.ScrolledText):
         if "state" in kwargs:
             self.state = kwargs["state"]
         else:
-            self.state = tk.NORMAL
+            self.state = NORMAL
 
         if self._textvariable is not None:
-            self.insert("1.0", self._textvariable.get())
-        self.tk.eval(
-            """
-            proc widget_proxy {widget widget_command args} {
-                set result [uplevel [linsert $args 0 $widget_command]]
-                if {([lindex $args 0] in {insert replace delete})} {
-                    event generate $widget <<Change>> -when tail
-                }
-                return $result
-            }
-            """
-        )
-        self.tk.eval(
-            """
-            rename {widget} _{widget}
-            interp alias {{}} ::{widget} {{}} widget_proxy {widget} _{widget}
-        """.format(
-                widget=str(tk.Text.__str__(self))
-            )
-        )
-        self.bind("<<Change>>", self._on_widget_change)
-        self.bind(
-            "<Double-Button-1>",
-            lambda _: TextEditor(self._textvariable),
-        )
+            self.text.insert("1.0", self._textvariable.get())
 
-        if self._textvariable is not None:
-            self._textvariable.trace("wu", self._on_var_change)
+        self.text.bind("<KeyRelease>", self._on_widget_change)
+        self.text.bind("<Double-Button-1>", lambda _: TextEditor(self._textvariable))
+
+        # if self._textvariable is not None:
+        self._textvariable.trace("wu", self._on_var_change)
 
     def _on_var_change(self, *args):
         """
         Callback function for the textvariable trace.
         Writes the textvariable value to the text widget.
         """
-        state = self["state"]
-        self["state"] = tk.NORMAL
-        text_current = self.get("1.0", "end-1c")
-        var_current = self._textvariable.get()  # type: ignore
+        state = self.text["state"]
+        self.text["state"] = NORMAL
+        text_current = self.text.get("1.0", "end-1c")
+        var_current = self._textvariable.get()
         if text_current != var_current:
-            self.delete("1.0", "end")
-            self.insert("1.0", var_current)
-        self["state"] = state
+            self.text.delete("1.0", "end")
+            self.text.insert("1.0", var_current)
+        self.text["state"] = state
 
     def _on_widget_change(self, event=None):
         """
@@ -78,7 +69,7 @@ class ScrolledText(scrolledtext.ScrolledText):
         Writes the widget value to the textvariable.
         """
         if self._textvariable is not None:
-            self._textvariable.set(self.get("1.0", "end-1c"))
+            self._textvariable.set(self.text.get("1.0", "end-1c"))
 
     @property
     def state(self) -> str:
@@ -88,26 +79,17 @@ class ScrolledText(scrolledtext.ScrolledText):
     @state.setter
     def state(self, value: str) -> None:
         """Sets the state of the widget. State can either be 'normal' or 'disabled'."""
-        match value:
-            case tk.NORMAL:
-                self.configure(
-                    state=tk.NORMAL,
-                    foreground="#000000",
-                    background="#ffffff",
-                    cursor="xterm",
-                )
-                self.bind(
-                    "<Double-Button-1>",
-                    lambda _: TextEditor(self._textvariable),
-                )
-            case tk.DISABLED:
-                self.configure(
-                    state=tk.DISABLED,
-                    foreground="#6d6d6d",
-                    background="#f0f0f0",
-                    cursor="arrow",
-                )
-                self.unbind("<Double-Button-1>")
-            case _:
-                raise ValueError(f"Cannot set widget to state {value}")
+        if value == NORMAL:
+            self.text["state"] = NORMAL
+            self.text["cursor"] = "xterm"
+            self.text.bind(
+                "<Double-Button-1>",
+                lambda _: TextEditor(self._textvariable),
+            )
+        elif value == DISABLED:
+            self.text["state"] = DISABLED
+            self.text["cursor"] = "arrow"
+            self.text.unbind("<Double-Button-1>")
+        else:
+            raise ValueError(f"Cannot set widget to state {value}")
         self._state = value
